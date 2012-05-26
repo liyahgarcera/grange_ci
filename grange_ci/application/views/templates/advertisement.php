2e8bf67c1561806637bb9b989bc86ead8be0b8f7
Brian Tria <btria959@gmail.com>
2012-05-26 19:50:32 +0800

    modified:   css/ad_slider.css 	modified:   css/forms.css 	modified:   css/grange_template.css 	modified:   grange_ci/application/config/autoload.php 	modified:   grange_ci/application/views/templates/advertisement.php 	modified:   grange_ci/application/views/templates/login_form.php

--- grange_ci/application/views/templates/advertisement.php
+++ grange_ci/application/views/templates/advertisement.php
@@ -1,4 +1,4 @@
 	<div id="advertisement">
-  	ad slider
+  	<marquee scrollamount="3">advertisement slider. joke lang ang marque.</marquee>
   </div><!-- end #advertisement -->
 </div><!-- end #sidebar -->
--- grange_ci/application/views/templates/login_form.php
+++ grange_ci/application/views/templates/login_form.php
@@ -1,6 +1,52 @@
 <div id="sidebar">
   <div id="login_form">
   <?php
-    echo "login ka dito."
-  ?>
+		$attr = array(
+				'id' => 'grange_login',
+				'name' => 'grange_login'
+			);
+  	echo form_open('display/view_home', $attr);
+			$attr = array(
+					'id' => 'label_for_username',
+					'name' => 'label_for_username'
+				);
+			echo form_label('Username:', 'username', $attr);
+			
+			$attr = array(
+					'id' => 'username',
+					'name' => 'username',
+					'maxlength' => '100',
+					'size' => '20'
+				);
+			echo form_input($attr);
+			
+			$attr = array(
+					'id' => 'label_for_password',
+					'name' => 'label_for_password'
+				);
+			echo form_label('Password:', 'password', $attr);
+			
+			$attr = array(
+					'id' => 'password',
+					'name' => 'password',
+					'maxlength' => '100',
+					'size' => '20'
+				);
+			echo form_password($attr);
+			
+			$attr = array(
+					'id' => 'sign_up_button',
+					'name' => 'sign_up_button',
+					'content' => 'Sign Up'
+				);
+			echo form_button($attr);
+			
+			$attr = array(
+					'id' => 'login_button',
+					'name' => 'login_button',
+					'value' => 'Login'
+				);
+			echo form_submit($attr);
+		echo form_close();
+	?>
   </div><!-- end #login_form -->
